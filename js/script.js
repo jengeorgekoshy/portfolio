6068e6641b0430bc8e9a1f9317c46075ccdc15c8
Jen George Koshy <jengeorgekoshy333@gmail.com>
2021-06-25 19:17:17 +0530

    form submission

--- js/script.js
+++ js/script.js
@@ -30,3 +30,21 @@ window.addEventListener("scroll", () => {
 })
 
 */
+
+$("#submit-form").submit((e)=>{
+  e.preventDefault()
+  $.ajax({
+      url:"https://script.google.com/macros/s/AKfycbycZr61CEe_6g3SUsbMrrortRy4-YUbqaJSpVCYUg7UXSLx9NI9Dp3FlmX0bzzs5dq6/exec",
+      data:$("#submit-form").serialize(),
+      method:"post",
+      success:function (response){
+          alert("Form submitted successfully")
+          window.location.reload()
+          //window.location.href="https://google.com"
+      },
+      error:function (err){
+          alert("Something Error")
+
+      }
+  })
+})
